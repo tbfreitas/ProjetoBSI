3da2b5289e5e8808e1751e13ea8cc59b42dc60ff
tbfreitas <tarcisiodf23@gmail.com>
2016-05-16 21:59:01 -0300

    Teste

--- app/Teams/directives/block/block.js
+++ app/Teams/directives/block/block.js
@@ -1,7 +1,14 @@
 app.directive('block', function(){
 
 	return{
-		templateUrl : 'app/Teams/directives/block/block.html'
-		
+		restrict: 'AE',
+		replace : true ,
+		scope : {
+			team:'='
+		},
+		templateUrl : 'app/Teams/directives/block/block.html',
+		link: function (scope) {	      
+			
+	    }	
 	}
 })
